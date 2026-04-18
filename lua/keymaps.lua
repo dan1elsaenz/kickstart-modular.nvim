160e11b79cba2f7c6a70f6a8f28f998861466994
Daniel Sáenz <dsaenzobando@gmail.com>
2026-04-17 22:18:42 -0600

    feat: Update keymaps
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -64,4 +64,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function() vim.hl.on_yank() end,
 })
 
+-- Activate Oil
+vim.keymap.set('n', '-', '<cmd>Oil<CR>', { desc = 'Activate Oil' })
+
 -- vim: ts=2 sts=2 sw=2 et
