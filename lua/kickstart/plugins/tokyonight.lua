7ebc2d19abfa3eaba5e827aaf06f405ea5cf854e
Daniel Sáenz <dsaenzobando@gmail.com>
2026-08-14 18:35:04 -0600

    feat: Enable transparent background in tokyonight theme
    Signed-off-by: Daniel Sáenz <dsaenzobando@gmail.com>

--- lua/kickstart/plugins/tokyonight.lua
+++ lua/kickstart/plugins/tokyonight.lua
@@ -11,8 +11,11 @@ return {
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
+        transparent = true, -- Enable transparent background
         styles = {
           comments = { italic = false }, -- Disable italics in comments
+          sidebars = 'transparent',
+          floats = 'transparent',
         },
       }
 
